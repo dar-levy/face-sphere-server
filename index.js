40defc5857f23c4b22ade9a41b03ebea5d898d46
Dar Levy <darlevy8@gmail.com>
2024-06-02 01:13:31 +0300

    Implement /api/users/page/:page on dummy array

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ const users = [{
     id: 1,
     name: 'Dar Levy',
     city: 'Ramat Gan',
+    page: 1
 }, {
     id: 2,
     name: 'Bill Gates',
-    company: 'Microsoft'
+    company: 'Microsoft',
+    page: 1
 }, {
     id: 3,
     name: 'Tim Cook',
-    company: 'Apple'
+    company: 'Apple',
+    page: 2
 },
 ]
 
@@ -20,14 +23,18 @@ app.get('/', (req, res) => {
     res.send('Hello world');
 });
 
-app.get('/getUsers/:id', (req, res) => {
-    user = users.find(user => user.id === parseInt(req.params.id));
+app.get('/api/users/:id', (req, res) => {
+    userId = parseInt(req.params.id)
+    user = users.find(user => user.id === userId);
     if (!user) res.status(404).send('The user with the given ID was not found');
-    res.send(user);
+    else res.send(user);
 });
 
-app.get('/getUsers/:page', (req, res) => {
-    res.send(users)
+app.get('/api/users/page/:page', (req, res) => {
+    const page = parseInt(req.params.page);
+    const usersByPage = users.filter(user => user.page === page);
+    if (usersByPage.length === 0) res.status(404).send('Users with the given page were not found');
+    else res.send(usersByPage);
 });
 
 const port = process.env.PORT || 3000;
