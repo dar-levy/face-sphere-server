f35896e0b3d50d780ea6b70da59c7a144f8b93e0
Dar Levy <darlevy8@gmail.com>
2024-06-02 23:15:15 +0300

    Add user route to index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const profiles = require('./routes/profiles');
+const users = require('./routes/users');
 
 const app = express();
 
@@ -10,6 +11,7 @@ mongoose.connect('mongodb://localhost/face-sphere')
 
 app.use(express.json());
 app.use('/api/profiles', profiles);
+app.use('/api/users', users);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
