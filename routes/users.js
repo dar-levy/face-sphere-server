cd767afa0cda7b89cf5e0494b911c53dcd5f3f5e
Dar Levy <darlevy8@gmail.com>
2024-06-02 23:10:48 +0300

    Create get user route

---
+++ routes/users.js
@@ -0,0 +1,10 @@
+const auth = require("../middleware/auth");
+const _ = require("lodash");
+const { User, validate } = require("../models/user");
+const express = require("express");
+const router = express.Router();
+
+router.get("/me", auth, async (req, res) => {
+    const user = await User.findById(req.user._id).select("-password");
+    res.send(user);
+});
