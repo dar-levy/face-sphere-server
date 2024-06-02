a85f9c50706c99f6e38f976cf6a5e08bba8c128d
Dar Levy <darlevy8@gmail.com>
2024-06-02 14:36:21 +0300

    Connect to mongodb://localhost/face-sphere

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const mongoose = require('mongoose');
 const users = require('./routes/users');
+
 const app = express();
+
+mongoose.connect('mongodb://localhost/face-sphere')
+    .then(() => console.log(`Connected to mongodb://localhost/face-sphere...`))
+    .catch(err => console.error('Could not connect to MongoDB...'));
 
 app.use(express.json());
 app.use('/api/users', users);
