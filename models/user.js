2c41d1a02151952b6716c2e229afcae4c6a739b8
Dar Levy <darlevy8@gmail.com>
2024-06-02 22:33:36 +0300

    Create userSchema

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const config = require("config");
+const Joi = require("joi");
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 255,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024
+    },
+    isAdmin: Boolean
+});
