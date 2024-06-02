06f3c8bee5de2c087073c3cb2f7d18e8761b0d0c
Dar Levy <darlevy8@gmail.com>
2024-06-02 23:47:36 +0300

    Add auth route to index

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require('mongoose');
 const profiles = require('./routes/profiles');
 const users = require('./routes/users');
+const auth = require('./routes/auth');
 
 const app = express();
 
@@ -12,6 +13,7 @@ mongoose.connect('mongodb://localhost/face-sphere')
 app.use(express.json());
 app.use('/api/profiles', profiles);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
