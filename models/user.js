463f8e421562a77a2601027562d506ebd536cccd
Dar Levy <darlevy8@gmail.com>
2024-06-02 22:34:31 +0300

    Implement validateUser

--- models/user.js
+++ models/user.js
@@ -26,3 +26,23 @@ const userSchema = new mongoose.Schema({
 });
 
 const User = mongoose.model("User", userSchema);
+
+function validateUser(user) {
+    const schema = {
+        name: Joi.string()
+            .min(2)
+            .max(50)
+            .required(),
+        email: Joi.string()
+            .min(5)
+            .max(255)
+            .required()
+            .email(),
+        password: Joi.string()
+            .min(5)
+            .max(255)
+            .required()
+    };
+
+    return Joi.validate(user, schema);
+}
