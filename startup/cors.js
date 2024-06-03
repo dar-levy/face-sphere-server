580abd4d3a65c48db348e1e86c66d8bc9711c7ec
Dar Levy <darlevy8@gmail.com>
2024-06-03 03:21:06 +0300

    Implement corsOptions

--- startup/cors.js
+++ startup/cors.js
@@ -6,6 +6,17 @@ const allowedOrigins = [
     'https://www.facebook.com'
 ];
 
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+};
+
 module.exports = function(app) {
-    app.use(cors());
+    app.use(cors(corsOptions));
 };
