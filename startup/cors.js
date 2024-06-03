92ff83424da8490fdd835987f150858be055b397
Dar Levy <darlevy8@gmail.com>
2024-06-03 03:19:33 +0300

    Create allowed origins collection

--- startup/cors.js
+++ startup/cors.js
@@ -1,5 +1,11 @@
 const cors = require("cors");
 
+const allowedOrigins = [
+    'https://localhost',
+    'https://www.google.com',
+    'https://www.facebook.com'
+];
+
 module.exports = function(app) {
     app.use(cors());
 };
