ff8085feb05f782fccc419dedb9cca02fd77fa4f
Dar Levy <darlevy8@gmail.com>
2024-06-03 04:12:14 +0300

    Create swagger startup js

---
+++ startup/swagger.js
@@ -0,0 +1,25 @@
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Profiles API",
+            version: "1.0.0",
+            description: "A simple Express API to manage profiles",
+        },
+        servers: [
+            {
+                url: "http://localhost:3000",
+            },
+        ],
+    },
+    apis: ["./routes/*.js"], // Path to the API docs
+};
+
+const specs = swaggerJsDoc(options);
+
+module.exports = (app) => {
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+};
