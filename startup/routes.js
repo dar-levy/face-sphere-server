29fdf770e56512f3978085fd3ce46bae4d9f9e77
Dar Levy <darlevy8@gmail.com>
2024-06-03 04:13:40 +0300

    Swagger the app

--- startup/routes.js
+++ startup/routes.js
@@ -3,6 +3,7 @@ const profiles = require("../routes/profiles");
 const users = require("../routes/users");
 const auth = require("../routes/auth");
 const error = require("../middleware/error");
+const swaggerSetup = require("./swagger");
 
 module.exports = function(app) {
     app.use(express.json());
@@ -10,4 +11,5 @@ module.exports = function(app) {
     app.use('/api/users', users);
     app.use('/api/auth', auth);
     app.use(error);
+    swaggerSetup(app);
 }
