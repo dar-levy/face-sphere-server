330a45ce73e896ce2763cf44e2a1d7c41db1c816
Dar Levy <darlevy8@gmail.com>
2024-06-04 04:50:09 +0300

    Fix deprecation warnings

--- startup/db.js
+++ startup/db.js
@@ -4,6 +4,12 @@ const config = require('config');
 
 module.exports = function() {
   const db = config.get('db');
-  mongoose.connect(db)
-    .then(() => winston.info(`Connected to ${db}...`));
+  mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+      .then(() => winston.info(`Connected to ${db}...`))
+      .catch(err => winston.error('Could not connect to MongoDB...', err));
 }
